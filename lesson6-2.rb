0bc89279e51e5059e17a55ff5d6406102c2995eb
maaarilyn <maaarilyn2828@gmail.com>
2023-06-21 22:49:11 +0000

    [Update]2-6

--- lesson6-2.rb
+++ lesson6-2.rb
@@ -1,19 +1,26 @@
 puts "計算を始めます"
 puts "何回繰り返しますか？"
 
-x = gets.to_i
-y = gets.to_i
-
-puts "計算結果を出力します"
-puts "#{x}*#{y}=#{x*y}"
-puts "計算を終了します"
+input = gets.to_i
 
 i = 1
-while i <= 10 do
-  if i == 5
-    puts "処理を終了します"
-    break
-  end
-  puts i
+while i <= input do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+　x = gets.to_i
+  y = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}}"
+  puts "#{a}-#{b}=#{a - b}}"
+  puts "#{a}*#{b}=#{a * b}}"
+  puts "#{a}/#{b}=#{a / b}}"
+
   i += 1
-en
+end
+
+puts "計算を終了します"
